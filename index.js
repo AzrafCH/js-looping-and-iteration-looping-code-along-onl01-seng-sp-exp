cd5b6b8aece7e0179b830dca74bab8c6492f2110
Azraf Chowdhury <achowdhur@ltu.edu>
2021-06-01 02:47:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ function writeCards(namesArray, event) {
   for (let i = 0; i < namesArray.length; i++) {
     thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
   }
+  return thankYouCards; 
 }
